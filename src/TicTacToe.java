755c19b0a8888634a8b9572ae1892beb636829e2
Dawid Badora <dawidbadora@gmail.com>
2022-09-02 14:51:41 +0200

    Initzialization of first values

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,3 +1,58 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
 
-public class TicTacToe {
+
+public class TicTacToe implements ActionListener {
+
+    Random random = new Random();
+    JFrame frame = new JFrame();
+    JPanel title_panel = new JPanel();
+    JPanel button_panel = new JPanel();
+    JLabel textfield = new JLabel();
+    JButton[] buttons = new JButton[9];
+    boolean player1_turn;
+
+    TicTacToe(){
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(800, 800);
+        frame.getContentPane().setBackground(Color.cyan);
+        frame.setLayout(new BorderLayout());
+        frame.setVisible(true);
+
+        textfield.setBackground(new Color(25,25,25));
+        textfield.setForeground(new Color(25,255,0));
+        textfield.setFont(new Font("Ink Free", Font.BOLD,75));
+        textfield.setHorizontalAlignment(JLabel.CENTER);
+        textfield.setText("Tic-Tac-Toe");
+        textfield.setOpaque(true);
+
+        title_panel.setLayout(new BorderLayout());
+        title_panel.setBounds(0,0,800,100);
+
+        title_panel.add(textfield);
+        frame.add(title_panel);
+    }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+    public void firstTurn(){
+
+    }
+
+    public void check(){
+
+    }
+
+    public void xWins(int a, int b, int c){
+
+    }
+
+    public void yWins(int a, int b, int c){
+
+    }
 }
